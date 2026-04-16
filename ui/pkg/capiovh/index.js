e71bd3a524b8622e3245939d07c38347f3f4a92a
jniedergang <julien_niedergang@hotmail.com>
2026-04-16 08:58:09 +0200

    fix(ui): remove Vue.version log that crashes at load time

--- ui/pkg/capiovh/index.js
+++ ui/pkg/capiovh/index.js
@@ -1,8 +1,3 @@
-import Vue from 'vue';
-
-console.log('[CAPIOVH] Extension loading, Vue version:', Vue.version);
-
 export default function(plugin) {
-  console.log('[CAPIOVH] Plugin init', plugin);
   plugin.addProduct(require('./product'));
 }
